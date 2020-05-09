9fc0624cf34e0e14ddddc77f6c1238cba6eba8af
Jonathan Besomi <43236409+JonathanBesomi@users.noreply.github.com>
2020-05-09 20:41:26 +0200

    git hooks for testing

--- scripts/check.sh
+++ scripts/check.sh
@@ -1,11 +1,18 @@
 #!/bin/sh
 
+# if any command inside script returns error, exit and return that error
+set -e 
+
+# ensure that we're always inside the root of our application
+cd "${0%/*}/.."
 
 echo "Test doctest."
-cd ../tests
+cd tests
 ./test_doctest.sh
 cd ..
 
 python3 -m unittest discover -s tests -t .
 
 #npm run build
+
+echo "Failed!" && exit 1
---
+++ scripts/pre-commit.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+echo "Running pre-commit hook"
+./check.sh
+
+# $? stores exit value of the last command
+if [ $? -ne 0 ]; then
+ echo "All tests must pass before commit."
+ exit 1
+fi

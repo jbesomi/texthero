3a6b4016ff6e65c16580228d5230dec576cb7726
Jonathan Besomi <43236409+JonathanBesomi@users.noreply.github.com>
2020-05-09 20:53:42 +0200

    git hooks for testing 6

--- scripts/check.sh
+++ scripts/check.sh
@@ -6,11 +6,19 @@ set -e
 # ensure that we're always inside the root of our application
 cd "${0%/*}/.."
 
+echo "Formatting code."
+cd scripts
+./format.sh
+cd ..
+
+
 echo "Test doctest."
 cd tests
 ./test_doctest.sh
 cd ..
 
+
+echo "Unittest."
 python3 -m unittest discover -s tests -t .
 
 cd website

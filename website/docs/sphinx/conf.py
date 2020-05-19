1eca21344bdcc9b26c7842d2cde8f6ca361e66b6
Jonathan Besomi <43236409+JonathanBesomi@users.noreply.github.com>
2020-05-19 22:44:59 +0200

    Get rid of sphinx-autodoc-typehints

--- website/docs/sphinx/conf.py
+++ website/docs/sphinx/conf.py
@@ -12,17 +12,18 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('.'))
+
+sys.path.insert(0, os.path.abspath("."))
 
 
 # -- Project information -----------------------------------------------------
 
-project = 'Texthero'
-copyright = '2020, Jonathan Besomi'
-author = 'Jonathan Besomi'
+project = "Texthero"
+copyright = "2020, Jonathan Besomi"
+author = "Jonathan Besomi"
 
 # The full version, including alpha/beta/rc tags
-release = 'v1.0'
+release = "v1.0"
 
 
 # -- General configuration ---------------------------------------------------
@@ -31,19 +32,18 @@ release = 'v1.0'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.napoleon',
-    'sphinx_autodoc_typehints',
-    'recommonmark'
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "recommonmark",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -51,16 +51,22 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = "alabaster"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
 
 
 html_css_files = [
-    'css/custom.css',
+    "css/custom.css",
 ]
 
-source_suffix = ['.rst', '.md']
+source_parsers = {
+    ".md": "recommonmark.parser.CommonMarkParser",
+}
+
+autodoc_typehints = "none"
+
+source_suffix = [".rst", ".md"]

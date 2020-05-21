d6b01a20973098884813bffc3ed6b56b1cc8555d
Selim Alawwa <selimalawwa335@gmail.com>
2020-05-21 04:47:14 +0200

    Test case to remove brackets and has content

--- tests/test_preprocessing.py
+++ tests/test_preprocessing.py
@@ -123,3 +123,41 @@ class TestPreprocessing(PandasTestCase):
         self.assertEqual(type(stopwords.DEFAULT), set)
         self.assertEqual(type(stopwords.NLTK_EN), set)
         self.assertEqual(type(stopwords.SPACY_EN), set)
+
+    """
+    Has content
+    """
+
+    def test_has_content(self):
+        s = pd.Series(["c", np.nan, "\t\\n", " ", "", "has content", None])
+        s_true = pd.Series([True, False, False, False, False, True, False])
+        self.assertEqual(preprocessing.has_content(s), s_true)
+
+    """
+    Remove brackets
+    """
+
+    def test_remove_round_brackets(self):
+        s = pd.Series("Remove all (brackets)(){/}[]<>")
+        s_true = pd.Series("Remove all {/}[]<>")
+        self.assertEqual(preprocessing.remove_round_brackets(s), s_true)
+
+    def test_remove_curly_brackets(self):
+        s = pd.Series("Remove all (brackets)(){/}[]<> { }")
+        s_true = pd.Series("Remove all (brackets)()[]<> ")
+        self.assertEqual(preprocessing.remove_curly_brackets(s), s_true)
+    
+    def test_remove_square_brackets(self):
+        s = pd.Series("Remove all [brackets](){/}[]<>")
+        s_true = pd.Series("Remove all (){/}<>")
+        self.assertEqual(preprocessing.remove_square_brackets(s), s_true)
+
+    def test_remove_angle_brackets(self):
+        s = pd.Series("Remove all <brackets>(){/}[]<>")
+        s_true = pd.Series("Remove all (){/}[]")
+        self.assertEqual(preprocessing.remove_angle_brackets(s), s_true)
+
+    def test_remove_brackets(self):
+        s = pd.Series("Remove all [square_brackets]{/curly_brackets}(round_brackets)<angle_brackets>")
+        s_true = pd.Series("Remove all ")
+        self.assertEqual(preprocessing.remove_brackets(s), s_true)

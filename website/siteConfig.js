c5c46d6a1f4f266ced753d5529b474d490b21a8c
Jonathan Besomi <43236409+JonathanBesomi@users.noreply.github.com>
2020-05-26 12:40:03 +0200

    Add favicon

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -115,6 +115,8 @@ const siteConfig = {
   ogImage: 'img/undraw_online.svg',
   twitterImage: 'img/undraw_tweetstorm.svg',
 
+  favicon: 'img/favicon.png',
+
   // For sites with a sizable amount of content, set collapsible to true.
   // Expand/collapse the links and subcategories under categories.
   // docsSideNavCollapsible: true,

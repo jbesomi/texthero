5b90d6ab8eb60b96e16c6bc7884a95930973490a
bobfang1992 <44713534+bobfang1992@users.noreply.github.com>
2020-07-08 15:16:57 +0100

    Add black to be part of the CI (#39)

--- dataset/Superheroes NLP Dataset/create_dataset.py
+++ dataset/Superheroes NLP Dataset/create_dataset.py
@@ -43,72 +43,100 @@ df = pd.DataFrame(dataset)
 df.columns = df.columns.str.lower()
 # Clean dataset
 
-def clean_teams(df):
-    df['teams'] = df['teams'].astype(str)
-    df['teams'] = df['teams'].str.replace("\nNo teams added.", "no_team")
 
-    df['teams'] = df['teams'].str.replace("\n", "").str.strip()
+def clean_teams(df):
+    df["teams"] = df["teams"].astype(str)
+    df["teams"] = df["teams"].str.replace("\nNo teams added.", "no_team")
+
+    df["teams"] = df["teams"].str.replace("\n", "").str.strip()
     return df
+
 
 df = clean_teams(df)
 
-# lowercase all columns 
+# lowercase all columns
 df.columns = df.columns.str.lower().str.replace(" ", "_")
 
 
 # Rename columns
-df = df.rename(columns={'type_/_race': 'type_race'})
+df = df.rename(columns={"type_/_race": "type_race"})
 
-power_score = dict(intelligence='intelligence_score',
-                   strength='strength_score',
-                   speed='speed_score',
-                   durability='durability_score',
-                   power='power_score',
-                   combat='combat_score'
+power_score = dict(
+    intelligence="intelligence_score",
+    strength="strength_score",
+    speed="speed_score",
+    durability="durability_score",
+    power="power_score",
+    combat="combat_score",
 )
 
 df = df.rename(columns=power_score)
 
-df = df.rename(columns=dict(hist_content='history_text', powers_content='powers_text'))
+df = df.rename(columns=dict(hist_content="history_text", powers_content="powers_text"))
 
 # Reorder columns
-df = df[['name', 'real_name', 'full_name', 'overall_score', 'history_text', 'powers_text', 'intelligence_score',
-       'strength_score', 'speed_score', 'durability_score', 'power_score',
-       'combat_score', 'superpowers', 'alter_egos', 'aliases',
-       'place_of_birth', 'first_appearance', 'creator', 'alignment',
-       'occupation', 'base', 'teams', 'relatives', 'gender', 'type_race',
-       'height', 'weight', 'eye_color', 'hair_color', 'skin_color', 'img']]
+df = df[
+    [
+        "name",
+        "real_name",
+        "full_name",
+        "overall_score",
+        "history_text",
+        "powers_text",
+        "intelligence_score",
+        "strength_score",
+        "speed_score",
+        "durability_score",
+        "power_score",
+        "combat_score",
+        "superpowers",
+        "alter_egos",
+        "aliases",
+        "place_of_birth",
+        "first_appearance",
+        "creator",
+        "alignment",
+        "occupation",
+        "base",
+        "teams",
+        "relatives",
+        "gender",
+        "type_race",
+        "height",
+        "weight",
+        "eye_color",
+        "hair_color",
+        "skin_color",
+        "img",
+    ]
+]
 
 # Extract 'superpowers' data
 
 df_superpowers = (
-    df['superpowers']
-    .apply(pd.Series)
-    .stack()
-    .pipe(pd.get_dummies)
-    .sum(level=0)
-   
+    df["superpowers"].apply(pd.Series).stack().pipe(pd.get_dummies).sum(level=0)
 )
 
 # Keep only most 50 common superpowers
 common_superpowers = df_superpowers.sum(axis=0).sort_values().tail(50).index
 df_superpowers = df_superpowers[common_superpowers]
 df_superpowers.columns = df_superpowers.columns.str.lower().str.replace(" ", "_")
-df_superpowers = df_superpowers.add_prefix('has_')
+df_superpowers = df_superpowers.add_prefix("has_")
 
 df = df.join(df_superpowers)
 
 
 # Split aliases
-df['aliases'] = df['aliases'].str.split("\n")
+df["aliases"] = df["aliases"].str.split("\n")
 
 print(df.shape)
 
 # Keep only rows where 'history_text' or 'powers_text' is not null.
-df = df[~(df['history_text'].str.strip() == "") | ~(df['powers_text'].str.strip() == "")]
+df = df[
+    ~(df["history_text"].str.strip() == "") | ~(df["powers_text"].str.strip() == "")
+]
 
 print(df.shape)
 
 
 df.to_csv("./data/superheroes_nlp_dataset.csv", index=False)
-    

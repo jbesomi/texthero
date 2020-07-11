d50559d850add56735fc6a0ee35df2632720eadd
henrifroese <50276689+henrifroese@users.noreply.github.com>
2020-07-11 10:44:40 +0200

    Add count_sentences function to nlp.py (#51)
    * Add count_sentences function to nlp.py
    
    Also add tests for the function to test_nlp.py
    
    * Implement suggestions from pull request.
    
    Add more tests, change style (docstring, tests naming).
    Remove unicode-casting to avoid unexpected behaviour.
    
    * Add link to spacy documentation.
    
    Additionally update index tests, they're cleaner now.
    
    Co-authored-by: Henri Froese <henri.froese@yahoo.com>

--- tests/test_nlp.py
+++ tests/test_nlp.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 from texthero import nlp
 
 from . import PandasTestCase
@@ -36,3 +37,34 @@ class TestNLP(PandasTestCase):
             [[("Today", "NP", 0, 5), ("such a beautiful day", "NP", 9, 29)]]
         )
         self.assertEqual(nlp.noun_chunks(s), s_true)
+
+    """
+    Count sentences.
+    """
+
+    def test_count_sentences(self):
+        s = pd.Series("I think ... it counts correctly. Doesn't it? Great!")
+        s_true = pd.Series(3)
+        self.assertEqual(nlp.count_sentences(s), s_true)
+
+    def test_count_sentences_numeric(self):
+        s = pd.Series([13.0, 42.0])
+        self.assertRaises(TypeError, nlp.count_sentences, s)
+
+    def test_count_sentences_missing_value(self):
+        s = pd.Series(["Test.", np.nan])
+        self.assertRaises(TypeError, nlp.count_sentences, s)
+
+    def test_count_sentences_index(self):
+        s = pd.Series(["Test"], index=[5])
+        counted_sentences_s = nlp.count_sentences(s)
+        t_same_index = pd.Series([""], index=[5])
+
+        self.assertTrue(counted_sentences_s.index.equals(t_same_index.index))
+
+    def test_count_sentences_wrong_index(self):
+        s = pd.Series(["Test", "Test"], index=[5, 6])
+        counted_sentences_s = nlp.count_sentences(s)
+        t_different_index = pd.Series(["", ""], index=[5, 7])
+
+        self.assertFalse(counted_sentences_s.index.equals(t_different_index.index))

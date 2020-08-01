ac14b08cd51cf836779004364c4950eb8d2a4ffd
Henri Froese <henri.froese@yahoo.com>
2020-08-01 13:56:43 +0200

    Implement suggested changes
    - flatten tests -> representation
    - new suggested tests in test_representation

--- tests/test_representation.py
+++ tests/test_representation.py
@@ -32,10 +32,24 @@ parameterized way.
 # Define valid inputs / outputs / indexes for different functions.
 s_not_tokenized = pd.Series(["This is not tokenized!"])
 s_tokenized = pd.Series([["Test", "Test", "TEST", "!"], ["Test", "?", ".", "."]])
+s_tokenized_with_noncontinuous_index = pd.Series(
+    [["Test", "Test", "TEST", "!"], ["Test", "?", ".", "."]], index=[5, 7]
+)
+
 s_tokenized_output_index = pd.MultiIndex.from_tuples(
     [(0, "!"), (0, "TEST"), (0, "Test"), (1, "."), (1, "?"), (1, "Test")],
     names=["document", "word"],
 )
+
+s_tokenized_output_noncontinuous_index = pd.MultiIndex.from_tuples(
+    [(5, "!"), (5, "TEST"), (5, "Test"), (7, "."), (7, "?"), (7, "Test")],
+    names=["document", "word"],
+)
+
+s_tokenized_output_min_df_index = pd.MultiIndex.from_tuples(
+    [(0, "Test"), (1, "Test")], names=["document", "word"],
+)
+
 
 test_cases_vectorization = [
     # format: [function_name, function, correct output for tokenized input above, dtype of output]
@@ -52,6 +66,13 @@ test_cases_vectorization = [
         [1.405465, 1.405465, 2.000000, 2.810930, 1.405465, 1.000000],
         "float",
     ],
+]
+
+test_cases_vectorization_min_df = [
+    # format: [function_name, function, correct output for tokenized input above, dtype of output]
+    ["count", representation.count, [2, 1], "int"],
+    ["term_frequency", representation.term_frequency, [0.666667, 0.333333], "float",],
+    ["tfidf", representation.tfidf, [2.0, 1.0], "float",],
 ]
 
 
@@ -83,9 +104,58 @@ class AbstractRepresentationTest(PandasTestCase):
         pd.testing.assert_series_equal(s_true, result_s)
 
     @parameterized.expand(test_cases_vectorization)
+    def test_vectorization_noncontinuous_index_kept(
+        self, name, test_function, correct_output_values, int_or_float
+    ):
+        if int_or_float == "int":
+            s_true = pd.Series(
+                correct_output_values,
+                index=s_tokenized_output_noncontinuous_index,
+                dtype="int",
+            ).astype(pd.SparseDtype(np.int64, 0))
+        else:
+            s_true = pd.Series(
+                correct_output_values,
+                index=s_tokenized_output_noncontinuous_index,
+                dtype="float",
+            ).astype(pd.SparseDtype("float", np.nan))
+
+        result_s = test_function(s_tokenized_with_noncontinuous_index)
+
+        pd.testing.assert_series_equal(s_true, result_s)
+
+    @parameterized.expand(test_cases_vectorization_min_df)
+    def test_vectorization_min_df(
+        self, name, test_function, correct_output_values, int_or_float
+    ):
+        if int_or_float == "int":
+            s_true = pd.Series(
+                correct_output_values,
+                index=s_tokenized_output_min_df_index,
+                dtype="int",
+            ).astype(pd.SparseDtype(np.int64, 0))
+        else:
+            s_true = pd.Series(
+                correct_output_values,
+                index=s_tokenized_output_min_df_index,
+                dtype="float",
+            ).astype(pd.SparseDtype("float", np.nan))
+
+        result_s = test_function(s_tokenized, min_df=2)
+
+        pd.testing.assert_series_equal(s_true, result_s)
+
+    @parameterized.expand(test_cases_vectorization)
     def test_vectorization_not_tokenized_yet_warning(self, name, test_function, *args):
         with self.assertWarns(DeprecationWarning):  # check raise warning
             test_function(s_not_tokenized)
+
+    @parameterized.expand(test_cases_vectorization)
+    def test_vectorization_arguments_to_sklearn(self, name, test_function, *args):
+        try:
+            test_function(s_not_tokenized, max_features=1, min_df=1, max_df=1.0)
+        except TypeError:
+            self.fail("Sklearn arguments not handled correctly.")
 
     """
     Individual / special tests.
@@ -108,3 +178,60 @@ class AbstractRepresentationTest(PandasTestCase):
         ).astype("Sparse")
 
         self.assertEqual(representation.tfidf(s), s_true)
+
+    """
+    flatten.
+    """
+
+    def test_flatten(self):
+        index = pd.MultiIndex.from_tuples(
+            [("doc0", "Word1"), ("doc0", "Word3"), ("doc1", "Word2")],
+            names=["document", "word"],
+        )
+        s = pd.Series([3, np.nan, 4], index=index)
+
+        s_true = pd.Series(
+            [[3.0, 0.0, np.nan], [0.0, 4.0, 0.0]], index=["doc0", "doc1"],
+        )
+
+        pd.testing.assert_series_equal(
+            representation.flatten(s), s_true, check_names=False
+        )
+
+    def test_flatten_fill_missing_with(self):
+        index = pd.MultiIndex.from_tuples(
+            [("doc0", "Word1"), ("doc0", "Word3"), ("doc1", "Word2")],
+            names=["document", "word"],
+        )
+        s = pd.Series([3, np.nan, 4], index=index)
+
+        s_true = pd.Series(
+            [[3.0, "FILLED", np.nan], ["FILLED", 4.0, "FILLED"]],
+            index=["doc0", "doc1"],
+            name="document",
+        )
+
+        pd.testing.assert_series_equal(
+            representation.flatten(s, fill_missing_with="FILLED"),
+            s_true,
+            check_names=False,
+        )
+
+    def test_flatten_missing_row(self):
+        # Simulating a row with no features, so it's completely missing from
+        # the representation series.
+        index = pd.MultiIndex.from_tuples(
+            [("doc0", "Word1"), ("doc0", "Word3"), ("doc1", "Word2")],
+            names=["document", "word"],
+        )
+        s = pd.Series([3, np.nan, 4], index=index)
+
+        s_true = pd.Series(
+            [[3.0, 0.0, np.nan], [0.0, 4.0, 0.0], [0.0, 0.0, 0.0]],
+            index=["doc0", "doc1", "doc2"],
+            name="document",
+        )
+
+        pd.testing.assert_series_equal(
+            representation.flatten(s, index=s_true.index), s_true, check_names=False
+        )

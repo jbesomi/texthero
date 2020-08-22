5c4db2fdf92d051d661c576a6490582a9b42a8ec
Maximilian Krahn <maximilian.krahn@icloud.com>
2020-08-22 17:42:25 +0200

    Add tests for custom pandas setitem method

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -74,3 +74,32 @@ class TestHelpers(PandasTestCase):
         with warnings.catch_warnings():
             warnings.simplefilter("ignore")
             self.assertTrue(f(s).index.equals(s_true.index))
+
+    def test_pandas_set_item_normal(self):
+        df1 = pd.DataFrame([[1, 2], [5, 3]], columns=["Test", "Test2"])
+        df2 = pd.DataFrame([0, 1])
+
+        df1["here"] = df2
+
+        pd.testing.assert_frame_equal(
+            df1,
+            pd.DataFrame([[1, 2, 0], [5, 3, 1]], columns=["Test", "Test2", "here"]),
+        )
+
+    def test_pandas_set_item_multiIndex(self):
+        df1 = pd.DataFrame(["Text 1", "Text 2"], columns=["Test"])
+        df2 = pd.DataFrame(
+            [[3, 5], [8, 4]], columns=pd.MultiIndex.from_product([["count"], [0, 1]]),
+        )
+
+        df1["here"] = df2
+
+        pd.testing.assert_frame_equal(
+            df1,
+            pd.DataFrame(
+                [["Text 1", 3, 5], ["Text 2", 8, 4]],
+                columns=pd.MultiIndex.from_tuples(
+                    [("Test", ""), ("here", 0), ("here", 1)]
+                ),
+            ),
+        )

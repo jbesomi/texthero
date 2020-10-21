c408c670374480c5d4951c84e921578f1ff0fd3c
Hugo Abonizio <hugo_abonizio@hotmail.com>
2020-10-21 16:26:32 -0300

    Add HTML tags removing to default preprocessing pipeline (#192)
    * Add HTML tags removing to default preprocessing pipeline
    
    * Update docstring and add more tests to default cleaning pipeline
    
    * Update `get_default_pipeline` docstring

--- texthero/preprocessing.py
+++ texthero/preprocessing.py
@@ -389,15 +389,17 @@ def get_default_pipeline() -> List[Callable[[pd.Series], pd.Series]]:
      1. :meth:`texthero.preprocessing.fillna`
      2. :meth:`texthero.preprocessing.lowercase`
      3. :meth:`texthero.preprocessing.remove_digits`
-     4. :meth:`texthero.preprocessing.remove_punctuation`
-     5. :meth:`texthero.preprocessing.remove_diacritics`
-     6. :meth:`texthero.preprocessing.remove_stopwords`
-     7. :meth:`texthero.preprocessing.remove_whitespace`
+     4. :meth:`texthero.preprocessing.remove_html_tags`
+     5. :meth:`texthero.preprocessing.remove_punctuation`
+     6. :meth:`texthero.preprocessing.remove_diacritics`
+     7. :meth:`texthero.preprocessing.remove_stopwords`
+     8. :meth:`texthero.preprocessing.remove_whitespace`
     """
     return [
         fillna,
         lowercase,
         remove_digits,
+        remove_html_tags,
         remove_punctuation,
         remove_diacritics,
         remove_stopwords,
@@ -415,10 +417,11 @@ def clean(s: TextSeries, pipeline=None) -> TextSeries:
      1. :meth:`texthero.preprocessing.fillna`
      2. :meth:`texthero.preprocessing.lowercase`
      3. :meth:`texthero.preprocessing.remove_digits`
-     4. :meth:`texthero.preprocessing.remove_punctuation`
-     5. :meth:`texthero.preprocessing.remove_diacritics`
-     6. :meth:`texthero.preprocessing.remove_stopwords`
-     7. :meth:`texthero.preprocessing.remove_whitespace`
+     4. :meth:`texthero.preprocessing.remove_html_tags`
+     5. :meth:`texthero.preprocessing.remove_punctuation`
+     6. :meth:`texthero.preprocessing.remove_diacritics`
+     7. :meth:`texthero.preprocessing.remove_stopwords`
+     8. :meth:`texthero.preprocessing.remove_whitespace`
 
     Parameters
     ----------

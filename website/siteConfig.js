aeb39025e77f073c3e8fe3755ad184c277a9c84e
Jonathan <jonathanbesomi@gmail.com>
2023-08-29 10:13:15 +0200

    change google analytics tracking id

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -8,62 +8,60 @@
 // See https://docusaurus.io/docs/site-config for all the possible
 // site configuration options.
 
-const path = require('path');
-
+const path = require("path");
 
 // List of projects/orgs using your project for the users page.
 const users = [
   {
-    caption: 'jbesomi',
+    caption: "jbesomi",
     // You will need to prepend the image path with your baseUrl
     // if it is not '/', like: '/test-site/img/image.jpg'.
-    image: '/img/undraw_open_source.svg',
-    infoLink: 'https://texthero.org',
+    image: "/img/undraw_open_source.svg",
+    infoLink: "https://texthero.org",
     pinned: true,
   },
 ];
 
 const siteConfig = {
-  title: 'Texthero', // Title for your website.
-  tagline: 'Text preprocessing, representation and visualization from zero to hero.',
-  url: 'https://texthero.org', // Your website URL
-  baseUrl: '/', // Base URL for your project */
+  title: "Texthero", // Title for your website.
+  tagline:
+    "Text preprocessing, representation and visualization from zero to hero.",
+  url: "https://texthero.org", // Your website URL
+  baseUrl: "/", // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'texthero',
-  organizationName: 'jbesomi',
+  projectName: "texthero",
+  organizationName: "jbesomi",
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
 
   // For no header links in the top nav bar -> headerLinks: [],
   headerLinks: [
-    {doc: 'getting-started', label: 'Getting started'},
-    {blog: true, label: 'Tutorial'},
-    {doc: 'api-preprocessing', label: 'API'},
+    { doc: "getting-started", label: "Getting started" },
+    { blog: true, label: "Tutorial" },
+    { doc: "api-preprocessing", label: "API" },
     {
-      href: 'https://github.com/jbesomi/texthero',
-      label: 'GitHub',
+      href: "https://github.com/jbesomi/texthero",
+      label: "GitHub",
     },
-
   ],
 
-  blogSidebarTitle: { default: 'Recent tutorials', all: 'All tutorials posts' },
+  blogSidebarTitle: { default: "Recent tutorials", all: "All tutorials posts" },
 
   // If you have users set above, you add it here:
   users,
 
   /* path to images for header/footer */
 
+  customDocsPath: path.basename(__dirname) + "/docs",
 
-  customDocsPath: path.basename(__dirname) + '/docs',
-
-  usePrism: ['py'],
+  usePrism: ["py"],
   highlight: {
-    theme: 'atom-one-dark',
+    theme: "atom-one-dark",
   },
 
   /*
@@ -74,8 +72,8 @@ const siteConfig = {
 
   /* Colors for website */
   colors: {
-    primaryColor: '#3f88c5',
-    secondaryColor: '#ff8c42',
+    primaryColor: "#3f88c5",
+    secondaryColor: "#ff8c42",
   },
 
   /* Custom fonts for website */
@@ -95,29 +93,28 @@ const siteConfig = {
   // This copyright info is used in /core/Footer.js and blog RSS/Atom feeds.
   copyright: `Texthero - MIT license`,
 
-
   // Add custom scripts here that would be placed in <script> tags.
   scripts: [
-    'https://buttons.github.io/buttons.js',
-    'https://cdnjs.cloudflare.com/ajax/libs/clipboard.js/2.0.0/clipboard.min.js',
-    '/js/code-block-buttons.js',
+    "https://buttons.github.io/buttons.js",
+    "https://cdnjs.cloudflare.com/ajax/libs/clipboard.js/2.0.0/clipboard.min.js",
+    "/js/code-block-buttons.js",
     "//cdnjs.cloudflare.com/ajax/libs/highlight.js/10.0.3/highlight.min.js",
-    '/js/start_highlight.js',
-   ],
-  stylesheets: ['/css/code-block-buttons.css', '/css/sphinx_basic.css'],
+    "/js/start_highlight.js",
+  ],
+  stylesheets: ["/css/code-block-buttons.css", "/css/sphinx_basic.css"],
 
   // On page navigation for the current documentation page.
-  onPageNav: 'separate',
+  onPageNav: "separate",
   // No .html extensions for paths.
   cleanUrl: true,
 
-  gaTrackingId: 'UA-164631738-1',
+  gaTrackingId: "G-F9P2X3MWVG",
 
   // Open Graph and Twitter card images.
-  ogImage: 'img/T.png',
-  twitterImage: 'img/T.png',
+  ogImage: "img/T.png",
+  twitterImage: "img/T.png",
 
-  favicon: 'img/favicon.png',
+  favicon: "img/favicon.png",
 
   // For sites with a sizable amount of content, set collapsible to true.
   // Expand/collapse the links and subcategories under categories.
